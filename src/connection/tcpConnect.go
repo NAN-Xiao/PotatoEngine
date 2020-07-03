5af0a91ed656fcf56f37d0938a1c6b6c3353ab74
肖楠 <xiaonan@xiaonandeMacBook-Pro.local>
2020-07-03 19:41:19 +0800

    修改tcp的链接

--- src/connection/tcpConnect.go
+++ src/connection/tcpConnect.go
@@ -11,7 +11,7 @@ type TcpConnect struct {
 	conn []*net.TCPConn
 }
 
-func (this *TcpConnect) Read()(l int,err error) {
+func (this *TcpConnect) Read() (l int, err error) {
 	go func() {
 
 	}()
@@ -21,48 +21,58 @@ func (this *TcpConnect) Write(data []byte) {
 
 }
 func (this *TcpConnect) Close() bool {
- return false
+	return false
 }
+
+//整个server监听
 func (this *TcpConnect) Listen() {
 	go func() {
+
 		addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:9000")
 		if err != nil {
 			println(err)
 			return
 		}
 		lisener, err := net.ListenTCP("tcp", addr)
-		c, err := lisener.AcceptTCP()
-		if err!=nil{
-			return
-		}
-		println("new client")
-		this.conn=append(this.conn,c)
-
-		go func(conn *net.TCPConn) {
-			println("tcp listening")
-			for {
-				var buf = make([]byte, 4)
-				n ,_:= io.ReadFull(conn,buf)
-				if n<4{
-					continue
-				}
-				len := binary.BigEndian.Uint32(buf)-4
-				buf = make([]byte, len)
-				n,_=io.ReadFull(conn,buf)
-				if n<4{
-					continue
-				}
-				//if err!=nil{
-				//	continue
-				//}
-				id, obj := netmessage.UnPackNetMessage(buf)
-				if id < 0 || obj == nil {
-					//消息错误
-					continue
-				}
-				//todo 接受数据分发消息
-				println("message")
+		for {
+			c, err := lisener.AcceptTCP()
+			if err != nil {
+				println(err)
+				return
 			}
-		}(c)
+			println("new client")
+			this.conn = append(this.conn, c)
+
+			go func(conn *net.TCPConn) {
+				println("tcp listening")
+				for {
+					var buf = make([]byte, 4)
+					n, err := io.ReadFull(conn, buf)
+					if err == io.EOF {
+						break
+					}
+					if n < 4 {
+						continue
+					}
+					len := binary.BigEndian.Uint32(buf) - 4
+					buf = make([]byte, len)
+					n, _ = io.ReadFull(conn, buf)
+					if n < 4 {
+						continue
+					}
+					id, obj := netmessage.UnPackNetMessage(buf)
+					if id < 0 || obj == nil {
+						//消息错误
+						continue
+					}
+					//todo 接受数据分发消息
+					println("message")
+				}
+				conn.Close()
+				println("CLose tcp con")
+			}(c)
+		}
+
 	}()
+
 }
--- testcast/gameserver/client/gameclient.go
+++ testcast/gameserver/client/gameclient.go
@@ -9,21 +9,21 @@ import (
 
 func main() {
 	post := "0.0.0.0:9000"
-	addr,err:=net.ResolveTCPAddr("tcp",post)
-	if err!=nil{
+	addr, err := net.ResolveTCPAddr("tcp", post)
+	if err != nil {
 		return
 	}
-	conn,err:=net.DialTCP("tcp",nil,addr)
-//	defer conn.Close()
-	if err!=nil{
+	conn, err := net.DialTCP("tcp", nil, addr)
+	//	defer conn.Close()
+	if err != nil {
 		return
 	}
-	request:=&message.LoginResquest{
+	request := &message.LoginResquest{
 		Username: "xiaonan",
 		Password: "123456",
 	}
-	data,err:=netmessage.PackageNetMessage(request)
-	if err!=nil{
+	data, err := netmessage.PackageNetMessage(request)
+	if err != nil {
 		return
 	}
 	fmt.Println(data)
