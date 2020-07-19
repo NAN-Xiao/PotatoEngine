5b14f5ec6318494e57c88aa0dbf7697749e3fa9e
肖楠 <nan.xiao@puzzlegamesplus.com>
2020-07-19 18:02:11 +0800

    test interface

--- testcast/testnother/testinterface/test.go
+++ testcast/testnother/testinterface/test.go
@@ -1,16 +1,16 @@
-package testinterface
+package main
+
+import "reflect"
 
 type Ispace interface {
 	CC()
-
 }
 
 type BaseSpace struct {
-
 	Name string
 }
 func(this *BaseSpace)DD(){
-
+ println("dd")
 }
 
 type MyClass struct {
@@ -18,6 +18,15 @@ type MyClass struct {
 	Ispace
 }
 
-func Test()  {
+func main()  {
+	m:=new(MyClass)
+   TestIN(m)
+}
+
+func TestIN(in Ispace){
+	t:=reflect.ValueOf(in)
+
+	fn:=t.MethodByName("DD")
+	fn.Call(nil)
 
 }
